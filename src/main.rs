c83dff1c2ea458d67297b8b331b7778b06db6a79
xwang <xion36325@gmail.com>
2026-02-14 01:59:03 +0530

    add code and more modularity

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
-use std::env;
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // turn iter to collection vector
 
-    let query = &args[1];
-    let file_path = &args[2];
-    
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{contents}");
+
+    // here we use search
+    Ok(())
+}
+
+struct Config<'a> {
+    query: &'a String,
+    file_path: &'a String,
+}
+impl Config<'_> {
+    fn build(args: &'_ [String]) -> Result<Config<'_>, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = &args[1];
+        let file_path = &args[2];
+
+        Ok(Config { query, file_path })
+    }
 }
