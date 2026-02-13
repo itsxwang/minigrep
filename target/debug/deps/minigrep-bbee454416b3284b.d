365d2ff88e2894ebb737c8e143e5ff1a0578d433
xwang <xion36325@gmail.com>
2026-02-13 22:24:32 +0530

    add gitignore

--- target/debug/deps/minigrep-bbee454416b3284b.d
+++ target/debug/deps/minigrep-bbee454416b3284b.d
@@ -1,5 +1,5 @@
-/home/xwang/Documents/grep-rs/target/debug/deps/minigrep-bbee454416b3284b.d: src/main.rs
+/home/xwang/Documents/minigrep/target/debug/deps/minigrep-bbee454416b3284b.d: src/main.rs
 
-/home/xwang/Documents/grep-rs/target/debug/deps/libminigrep-bbee454416b3284b.rmeta: src/main.rs
+/home/xwang/Documents/minigrep/target/debug/deps/libminigrep-bbee454416b3284b.rmeta: src/main.rs
 
 src/main.rs:
