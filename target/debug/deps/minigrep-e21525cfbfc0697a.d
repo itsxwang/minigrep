9e256cf95d889be5236030305fa4bca133873f6c
Xion <121360201+itsxwang@users.noreply.github.com>
2026-02-13 22:26:04 +0530

    Delete target directory

--- target/debug/deps/minigrep-e21525cfbfc0697a.d
+++
@@ -1,5 +0,0 @@
-/home/xwang/Documents/minigrep/target/debug/deps/minigrep-e21525cfbfc0697a.d: src/main.rs
-
-/home/xwang/Documents/minigrep/target/debug/deps/libminigrep-e21525cfbfc0697a.rmeta: src/main.rs
-
-src/main.rs:
