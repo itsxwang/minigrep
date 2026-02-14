59d7ab790fa1aa2f1aac1415902dbe0abbfcd111
xwang <xion36325@gmail.com>
2026-02-14 17:00:09 +0530

    made code more organize, complete search functionality for also insensitive and smart search support

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
-pub fn search<'a>(s: &'a str, query: &'a str) -> Option<&'a str> {
-    if s.contains(query) { Some(s) } else { None }
+
+
+pub fn search_sensitive<'a>(content: &'a str, query: &str) -> Option<&'a str> {
+    if content.contains(query) {
+        Some(content)
+    } else {
+        None
+    }
+}
+
+pub fn search_case_insensitive<'a>(content: &'a str, query: &str) -> Option<&'a str> {
+    if content.to_lowercase().contains(&query.to_lowercase()) {
+        Some(content)
+    } else {
+        None
+    }
+}
+
+pub fn search_case_smart<'a>(content: &'a str, query: &str) -> Option<&'a str> {
+    //  iterate over each char of query and see is any char uppercase, if yes don't search case insensitive, use normal search
+    for c in query.chars() {
+        if c.is_uppercase() {
+            return search_sensitive(content, query);
+        }
+    }
+    search_case_insensitive(content, query)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use minigrep::search;
+use minigrep::{search_case_insensitive, search_case_smart, search_sensitive};
 use std::{env, error::Error, fs, process};
 
 fn main() {
@@ -10,7 +10,10 @@ fn main() {
     });
 
     println!(
-        "----------- Searching for {} in {} ----------- ",
+        "SET ENV:\nCASE=0 => For Case INSENITIVE\nCASE=1 => For Case SMART\nCASE=2 (default) => For Case SENSITIVE\n",
+    );
+    println!(
+        "----------- Searching for `{}` in `{}` -----------\n",
         config.query, config.file_path
     );
 
@@ -23,25 +26,60 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     for line in contents.lines() {
-        if let Some(s) = search(line, config.query) {
-            println!("{s}");
+        match config.case {
+            "smart" => {
+                if let Some(s) = search_case_smart(line, config.query) {
+                    print_matches(s);
+                }
+            }
+            "insensitive" => {
+                if let Some(s) = search_case_insensitive(line, config.query) {
+                    print_matches(s);
+                }
+            }
+            _ => {
+                if let Some(s) = search_sensitive(line, config.query) {
+                    print_matches(s);
+                }
+            }
         }
     }
+
     Ok(())
 }
 
+fn print_matches(s: &str) {
+    // for now simply print the lines
+    println!("{}", s);
+}
 struct Config<'a> {
-    query: &'a String,
-    file_path: &'a String,
+    query: &'a str,
+    file_path: &'a str,
+    case: &'a str,
 }
 impl Config<'_> {
-    fn build(args: &'_ [String]) -> Result<Config<'_>, &'static str> {
+    fn build<'a>(args: &'a [String]) -> Result<Config<'a>, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
         let query = &args[1];
         let file_path = &args[2];
 
-        Ok(Config { query, file_path })
+        let case = match env::var("CASE") {
+            Ok(val) => {
+                match val.as_str() {
+                    "0" => "insensitive",
+                    "1" => "smart",
+                    _ => "sensitive",
+                }
+            }
+            Err(_) => "sensitive",
+        };
+
+        Ok(Config {
+            query,
+            file_path,
+            case,
+        })
     }
 }
